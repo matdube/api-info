4fae174f31b15f3fccf22cab701b2de18224af49
mathieu <matdube@gmail.com>
2022-02-27 17:29:27 -0500

    Add health probe

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 const http = require("http");
 const variables = require("./variables");
 
+const handleHealthProbe = (req, res) => {
+  if (req.url === "/health") {
+    res.writeHead(200, { "Content-Type": "application/text" });
+    res.end("OK");
+    return true;
+  }
+
+  return false;
+};
+
+const handleGetVariable = (req, res) => {
+  for (let i = 0; i < variables.length; i++) {
+    const variable = variables[i];
+
+    if (req.url.toUpperCase() === `/${variable.key}` && req.method === "GET") {
+      found = true;
+      res.writeHead(200, { "Content-Type": "application/text" });
+      res.end(variable.value);
+      return true;
+    }
+  }
+
+  return false;
+};
+
 const server = http.createServer(async (req, res) => {
-
-    let found = false;
-
-    for (let i = 0; i < variables.length; i++) {
-        const variable = variables[i];
-        
-        if (req.url.toUpperCase() === `/${variable.key}` && req.method === "GET") {
-            found = true;
-            res.writeHead(200, { "Content-Type": "application/text" });            
-            res.end(variable.value);
-        }
-    }
-
-    if (!found) {
-        res.writeHead(404, { "Content-Type": "application/text" });
-        res.end("Not found");
-    }
-
+  if (!(handleHealthProbe(req, res) || handleGetVariable(req, res))) {
+    res.writeHead(404, { "Content-Type": "application/text" });
+    res.end("Not found");
+  }
 });
 
 module.exports = server;
