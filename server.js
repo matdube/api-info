8e5d3d81b10b8e165a8926843bf75cf7e270ba24
mathieu <matdube@gmail.com>
2022-02-27 19:15:13 -0500

    Support custom path

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require("http");
 const variables = require("./variables");
 
+const path = process.env.PATH || "";
+
 const handleHealthProbe = (req, res) => {
-  if (req.url === "/health") {
+  if (req.url === `${path}/health` && req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/text" });
     res.end("OK");
     return true;
@@ -15,7 +17,7 @@ const handleGetVariable = (req, res) => {
   for (let i = 0; i < variables.length; i++) {
     const variable = variables[i];
 
-    if (req.url.toUpperCase() === `/${variable.key}` && req.method === "GET") {
+    if (req.url.toUpperCase() === `${path.toUpperCase()}/${variable.key}` && req.method === "GET") {
       found = true;
       res.writeHead(200, { "Content-Type": "application/text" });
       res.end(variable.value);
